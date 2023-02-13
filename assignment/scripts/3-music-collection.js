0a2fd14a95029bc1fbe0bc78a078c87f92e16bbf
Steven Gangl <steven.h.gangl@gmail.com>
2023-02-13 17:12:13 -0600

    first 1 done

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,23 @@
 console.log('***** Music Collection *****')
+
+let collection = []
+function addToCollection(title, artist, yearPublished) {
+    let music = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    }
+    collection.push(music)
+    return music;
+}
+addToCollection('dog','cat', 'fox')
+console.log('this is the collections array', collection)
+
+
+
+
+
+
+
+
+
