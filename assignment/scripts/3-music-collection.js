f2874eb3945753942e3a04f7a13460dd57e493b1
Steven Gangl <steven.h.gangl@gmail.com>
2023-02-13 17:32:27 -0600

    half done

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -10,14 +10,19 @@ function addToCollection(title, artist, yearPublished) {
     collection.push(music)
     return music;
 }
-addToCollection('dog','cat', 'fox')
-console.log('this is the collections array', collection)
+addToCollection('dog', 'cat', 10)
 
+console.log('adding to the collection array', addToCollection('dog1', 'cat1', 11))
 
+console.log('2nd added', addToCollection('dog2', 'cat2', 12))
 
+console.log('3rd added', addToCollection('dog3', 'cat3', 13))
 
+console.log('4th added', addToCollection('dog4', 'cat4', 14))
 
+console.log('5th added', addToCollection('dog5', 'cat5', 15))
 
+console.log('6th added', addToCollection('dog6', 'cat6', 16))
 
-
+console.log('collection array', collection);
 
